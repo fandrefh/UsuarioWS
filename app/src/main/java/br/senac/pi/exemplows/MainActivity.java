03e8186ce526e241d4ef714ab17ba10f8297215d
Francisco André <fandrefh@gmail.com>
2015-12-11 19:58:31 -0200

    Integracao android e webservice

--- app/src/main/java/br/senac/pi/exemplows/MainActivity.java
+++ app/src/main/java/br/senac/pi/exemplows/MainActivity.java
@@ -1,13 +1,73 @@
 package br.senac.pi.exemplows;
 
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtNome, edtIdade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        UsuarioWS usuarioWS = new UsuarioWS();
+        /*boolean resultado = usuarioWS.inserirUsuario(new Usuario(0, "Outro teste qualquer", 23));
+        Log.d("cursoandroid", "Deu certo? " + resultado);*/
+
+        /*ArrayList<Usuario> lista = usuarioWS.buscarTodosUsuario();
+        Log.d("cursoandroid", "Usuários: " + lista.toString());*/
+
+        Usuario u = usuarioWS.buscarPorID(1);
+        Log.d("cursoandroid", "Usuário: " + u.toString());
+
+        boolean r1 = usuarioWS.excluirUsuario(new Usuario(3, "", 0));
+        Log.d("cursoandroid", "Deu certo? " + r1);
+
+        boolean r2 = usuarioWS.atualizarUsuario(new Usuario(1, "Atualizado", 26));
+        Log.d("cursoandroid", "Deu certo? " + r2);
+
+
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        edtIdade = (EditText) findViewById(R.id.edtIdade);
+        findViewById(R.id.btnCadastrar).setOnClickListener(cadastrar());
+    }
+
+    private View.OnClickListener cadastrar() {
+        return new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String nome = edtNome.getText().toString();
+                int idade = Integer.parseInt(edtIdade.getText().toString());
+                UsuarioWS usuarioWS = new UsuarioWS();
+                boolean resultado = usuarioWS.inserirUsuario(new Usuario(0, nome, idade));
+                if (resultado) {
+                    userMessage(getString(R.string.cadastro_sucesso));
+                    edtNome.setText("");
+                    edtIdade.setText("");
+                    edtNome.requestFocus();
+                } else {
+                    userMessage(getString(R.string.cadastro_erro));
+                }
+            }
+        };
+    }
+
+    private void userMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/br/senac/pi/exemplows/UsuarioWS.java
+++ app/src/main/java/br/senac/pi/exemplows/UsuarioWS.java
@@ -5,6 +5,9 @@ import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 /**
  * Created by Note on 04/12/2015.
  */
@@ -23,15 +26,107 @@ public class UsuarioWS {
         try {
             SoapObject soapObject = new SoapObject(NAMESPACE, INSERIR);
             SoapObject user = new SoapObject(NAMESPACE, "usuario");
-            soapObject.addProperty("id", usuario.getId());
-            soapObject.addProperty("nome", usuario.getNome());
-            soapObject.addProperty("idade", usuario.getIdade());
+            user.addProperty("id", usuario.getId());
+            user.addProperty("nome", usuario.getNome());
+            user.addProperty("idade", usuario.getIdade());
             soapObject.addSoapObject(user);
             SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
             envelope.setOutputSoapObject(soapObject);
             envelope.implicitTypes = true;
             HttpTransportSE transportSE = new HttpTransportSE(URL);
             transportSE.call("urn:" + INSERIR, envelope);
+            Object resposta = envelope.getResponse();
+            return Boolean.parseBoolean(resposta.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public ArrayList<Usuario> buscarTodosUsuario() {
+        ArrayList<Usuario> lista = new ArrayList<Usuario>();
+        SoapObject soapObject = new SoapObject(NAMESPACE, BUSCAR_TODOS);
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        envelope.setOutputSoapObject(soapObject);
+        envelope.implicitTypes = true;
+        HttpTransportSE transportSE = new HttpTransportSE(URL);
+        try {
+            transportSE.call("urn:" + BUSCAR_TODOS, envelope);
+            Vector<SoapObject> resposta = (Vector<SoapObject>) envelope.getResponse();
+            for (SoapObject so: resposta) {
+                Usuario user = new Usuario();
+                user.setId(Integer.parseInt(so.getProperty("id").toString()));
+                user.setNome(so.getProperty("nome").toString());
+                user.setIdade(Integer.parseInt(so.getProperty("idade").toString()));
+                lista.add(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return lista;
+    }
+
+    public Usuario buscarPorID(int id) {
+        Usuario usuario = null;
+        SoapObject soapObject = new SoapObject(NAMESPACE, BUSCAR_POR_ID);
+        soapObject.addProperty("id", id);
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+        envelope.setOutputSoapObject(soapObject);
+        envelope.implicitTypes = true;
+        HttpTransportSE transportSE = new HttpTransportSE(URL);
+        try {
+            transportSE.call("urn:" + BUSCAR_POR_ID, envelope);
+            SoapObject resposta = (SoapObject) envelope.getResponse();
+            usuario = new Usuario();
+            usuario.setId(Integer.parseInt(resposta.getProperty("id").toString()));
+            usuario.setNome(resposta.getProperty("nome").toString());
+            usuario.setIdade(Integer.parseInt(resposta.getProperty("idade").toString()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return usuario;
+        }
+        return usuario;
+    }
+
+    public boolean excluirUsuario(Usuario usuario) {
+        try {
+            SoapObject soapObject = new SoapObject(NAMESPACE, EXCLUIR);
+            SoapObject user = new SoapObject(NAMESPACE, "usuario");
+            user.addProperty("id", usuario.getId());
+            user.addProperty("nome", usuario.getNome());
+            user.addProperty("idade", usuario.getIdade());
+            soapObject.addSoapObject(user);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.setOutputSoapObject(soapObject);
+            envelope.implicitTypes = true;
+            HttpTransportSE transportSE = new HttpTransportSE(URL);
+            transportSE.call("urn:" + EXCLUIR, envelope);
+            Object resposta = envelope.getResponse();
+            return Boolean.parseBoolean(resposta.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean excluirUsuario(int id) {
+        return excluirUsuario(new Usuario(id, "", 0));
+    }
+
+    public boolean atualizarUsuario(Usuario usuario) {
+        try {
+            SoapObject soapObject = new SoapObject(NAMESPACE, ATUALIZAR);
+            SoapObject user = new SoapObject(NAMESPACE, "usuario");
+            user.addProperty("id", usuario.getId());
+            user.addProperty("nome", usuario.getNome());
+            user.addProperty("idade", usuario.getIdade());
+            soapObject.addSoapObject(user);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.setOutputSoapObject(soapObject);
+            envelope.implicitTypes = true;
+            HttpTransportSE transportSE = new HttpTransportSE(URL);
+            transportSE.call("urn:" + ATUALIZAR, envelope);
             Object resposta = envelope.getResponse();
             return Boolean.parseBoolean(resposta.toString());
         } catch (Exception e) {
